4260d080caa7b167235c4b1f8f2156e0bfddaf90
Daniel Kelly <me@danielkelly.io>
2021-06-22 16:52:04 -0500

    3. create your first single file component

---
+++ src/components/BigYellowUsername.vue
@@ -0,0 +1,18 @@
+<template>
+  <p class="username">{{username}}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    username: { type: String, required: true }
+  }
+}
+</script>
+
+<style lang="css">
+  .username{
+    font-size: 50pt;
+    color: #DCAE1D;
+  }
+</style>
