29ab71cebfae122b7bbe427c3518199560401461
Daniel Kelly <me@danielkelly.io>
2021-06-22 17:13:14 -0500

    8. loading components async

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="hello">
     <h1 class="username">{{msg}}</h1>
-    <BigYellowUsername username="danielkellyio" />
+    <button @click="showUsername = !showUsername">Toggle Username</button>
+    <div v-if="showUsername">
+      <BigYellowUsername username="danielkellyio" />
+    </div>
   </div>
 </template>
 
 <script>
-import BigYellowUsername from './BigYellowUsername.vue'
+import { defineAsyncComponent } from 'vue'
 export default {
   name: 'HelloWorld',
-  components: { BigYellowUsername },
+  components: {
+    BigYellowUsername: () => defineAsyncComponent(import(/* webpackChunkName: "BigYellowUsername" */ './BigYellowUsername.vue'))
+  },
   props: {
     msg: String
+  },
+  data () {
+    return {
+      showUsername: false
+    }
   }
 }
 </script>
